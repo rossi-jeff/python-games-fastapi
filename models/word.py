0be4b2adc90fe3396005cc2f6d38feb99b744458
Jeff Rossi <rossi.jeff@gmail.com>
2023-08-11 13:30:23 -0400

    user, word models

---
+++ models/word.py
@@ -0,0 +1,11 @@
+from base import Base
+from sqlalchemy import Column, BigInteger, String, Integer, DateTime
+
+class Word(Base):
+    __tablename__ = "words"
+
+    id: Column(BigInteger, primary_key=True)
+    created_at: Column(DateTime)
+    updated_at: Column(DateTime)
+    Word: Column(String(30)) 
+    Length: Column(Integer)
