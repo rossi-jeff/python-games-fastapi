8e4341115613750832e929d3b00af8e29f7bb01a
Jeff Rossi <jrossi@intouchgps.com>
2023-08-18 12:45:09 -0400

    hang man

--- routes/hang_man_route.py
+++ routes/hang_man_route.py
@@ -6,6 +6,8 @@ from payloads.hang_man_payload import HangManCreate, HangManGuess
 from utilities.guess_word import ListContains
 from utilities.hang_man import CalculateHangManStatus, CalculateHangManScore, StringListUnique
 from models.enums import GameStatusArray
+from optional_auth import get_current_user
+from typing import Optional
 
 router = APIRouter(
     prefix="/api/hang_man",
@@ -25,6 +27,15 @@ async def hang_men_paginated(Limit: int = 10, Offset: int = 0, db: Session = Dep
         items.append(hm.as_dict())
     return {"Count": count, "Limit": Limit, "Offset": Offset, "Items": items}
 
+@router.get("/progress")
+async def hang_men_in_progress(db: Session = Depends(get_db), user_id: Optional[str] = Depends(get_current_user)):
+    items = []
+    if user_id is not None:
+        hang_men = db.query(HangMan).where(HangMan.Status == 1).filter(HangMan.user_id == user_id).all()
+        for hm in hang_men:
+            items.append(hm.as_dict())
+    return items
+
 @router.get("/{hang_man_id}")
 async def get_hang_man_by_id(hang_man_id: int, db: Session = Depends(get_db)):
     hang_man = db.query(HangMan).where(HangMan.id == hang_man_id).first()
@@ -33,7 +44,7 @@ async def get_hang_man_by_id(hang_man_id: int, db: Session = Depends(get_db)):
     return hang_man.as_dict()
 
 @router.post("/")
-async def create_hang_man(body: HangManCreate, db: Session = Depends(get_db)):
+async def create_hang_man(body: HangManCreate, db: Session = Depends(get_db), user_id: Optional[str] = Depends(get_current_user)):
     hang_man = HangMan(
         word_id = body.WordId,
         Status = 1,
@@ -41,6 +52,8 @@ async def create_hang_man(body: HangManCreate, db: Session = Depends(get_db)):
         Correct = "",
         Wrong = ""
     )
+    if user_id is not None:
+        hang_man.user_id = user_id
     db.add(hang_man)
     db.commit()
     db.refresh(hang_man)
