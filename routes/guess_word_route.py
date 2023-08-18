896283eb4b3a5d803b4f3b1c8e92d65311980e7f
Jeff Rossi <jrossi@intouchgps.com>
2023-08-18 12:36:23 -0400

    guess word

--- routes/guess_word_route.py
+++ routes/guess_word_route.py
@@ -13,6 +13,8 @@ from utilities.guess_word import (
     CalculateGuessRatings
 )
 from models.guess_word_guess import GuessWordGuess
+from optional_auth import get_current_user
+from typing import Optional
 
 router = APIRouter(
     prefix="/api/guess_word",
@@ -32,6 +34,15 @@ async def guess_words_paginated(Limit: int = 10, Offset: int = 0, db: Session = 
         items.append(gw.as_dict())
     return {"Count": count, "Limit": Limit, "Offset": Offset, "Items": items}
 
+@router.get("/progress")
+async def guess_words_in_progress(db: Session = Depends(get_db), user_id: Optional[str] = Depends(get_current_user)):
+    items = []
+    if user_id is not None:
+        guess_words = db.query(GuessWord).where(GuessWord.Status == 1).filter(GuessWord.user_id == user_id).all()
+        for gw in guess_words:
+            items.append(gw.as_dict())
+    return items
+
 @router.get("/{guess_word_id}")
 async def get_guess_word_by_id(guess_word_id: int, db: Session = Depends(get_db)):
     guess_word = db.query(GuessWord).where(GuessWord.id == guess_word_id).first()
@@ -40,12 +51,14 @@ async def get_guess_word_by_id(guess_word_id: int, db: Session = Depends(get_db)
     return guess_word.as_dict(True)
 
 @router.post("/")
-async def create_guess_word(body: GuessWordCreate, db: Session = Depends(get_db)):
+async def create_guess_word(body: GuessWordCreate, db: Session = Depends(get_db), user_id: Optional[str] = Depends(get_current_user)):
     guess_word = GuessWord(
         word_id = body.WordId,
         Score = 0,
         Status = 1
     )
+    if user_id is not None:
+        guess_word.user_id = user_id
     db.add(guess_word)
     db.commit()
     db.refresh(guess_word)
