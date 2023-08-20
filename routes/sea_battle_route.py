4073e32c5acd7cce2d74cb7303e9055121074461
Jeff Rossi <jrossi@intouchgps.com>
2023-08-20 10:23:08 -0400

    sea battle score

--- routes/sea_battle_route.py
+++ routes/sea_battle_route.py
@@ -6,7 +6,7 @@ from payloads.sea_battle_payload import SeaBattleCreate, SeaBattleShipPayload, S
 from models.sea_battle_ship import SeaBattleShip
 from models.enums import ShipTypeArray, NavyArray, Navy, Target, ShipType, TargetArray
 from models.sea_battle_ship_grid_point import SeaBattleShipGridPoint
-from utilities.sea_battle import OpponentShipPoints, OpponentFirePoint
+from utilities.sea_battle import OpponentShipPoints, OpponentFirePoint, SeaBattleStatus, UpdateSeaBattle
 from models.sea_battle_ship_hit import SeabattleShipHit
 from models.sea_battle_turn import SeaBattleTurn
 from optional_auth import get_current_user
@@ -21,10 +21,11 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+
 @router.get("/")
 async def sea_battles_paginated(
-            Limit: int = 10, Offset: int = 0, db: Session = Depends(get_db)
-        ) -> SeaBattlePaginatedResponse:
+    Limit: int = 10, Offset: int = 0, db: Session = Depends(get_db)
+) -> SeaBattlePaginatedResponse:
     count = db.query(SeaBattle).where(SeaBattle.Status != 1).count()
     sea_battles = db.query(SeaBattle).where(SeaBattle.Status != 1).order_by(
         SeaBattle.Status.desc(),
@@ -35,36 +36,41 @@ async def sea_battles_paginated(
         items.append(sb.as_dict())
     return {"Count": count, "Limit": Limit, "Offset": Offset, "Items": items}
 
+
 @router.get("/progress")
 async def sea_battles_in_progress(
-            db: Session = Depends(get_db), user_id: Optional[str] = Depends(get_current_user)
-        ) -> List[SeaBattleResponse]:
+    db: Session = Depends(get_db), user_id: Optional[str] = Depends(get_current_user)
+) -> List[SeaBattleResponse]:
     items = []
     if user_id is not None:
-        sea_battles = db.query(SeaBattle).where(SeaBattle.Status == 1).filter(SeaBattle.user_id == user_id).all()
+        sea_battles = db.query(SeaBattle).where(
+            SeaBattle.Status == 1).filter(SeaBattle.user_id == user_id).all()
         for sb in sea_battles:
             items.append(sb.as_dict())
     return items
 
+
 @router.get("/{sea_battle_id}")
 async def get_sea_battle_by_id(
-            sea_battle_id: int, db: Session = Depends(get_db)
-        ) -> SeaBattleResponse:
-    sea_battle = db.query(SeaBattle).where(SeaBattle.id == sea_battle_id).first()
+    sea_battle_id: int, db: Session = Depends(get_db)
+) -> SeaBattleResponse:
+    sea_battle = db.query(SeaBattle).where(
+        SeaBattle.id == sea_battle_id).first()
     if sea_battle is None:
         raise HTTPException(status_code=404, detail="Sea Battle not found")
     return sea_battle.as_dict(True)
 
+
 @router.post("/", status_code=201)
 async def create_sea_battle(
-            body: SeaBattleCreate, 
-            db: Session = Depends(get_db), 
-            user_id: Optional[str] = Depends(get_current_user)
-        ) -> SeaBattleResponse:
+    body: SeaBattleCreate,
+    db: Session = Depends(get_db),
+    user_id: Optional[str] = Depends(get_current_user)
+) -> SeaBattleResponse:
     sea_battle = SeaBattle(
-        Axis = body.Axis,
-        Status = 1,
-        Score = 0
+        Axis=body.Axis,
+        Status=1,
+        Score=0
     )
     if user_id is not None:
         sea_battle.user_id = user_id
@@ -73,19 +79,21 @@ async def create_sea_battle(
     db.refresh(sea_battle)
     return sea_battle.as_dict()
 
+
 @router.post("/{sea_battle_id}/ship", status_code=201)
 async def sea_battle_create_ship(
-            sea_battle_id: int, body: SeaBattleShipPayload, db: Session = Depends(get_db)
-        ) -> SeaBattleShipResponse:
-    sea_battle = db.query(SeaBattle).where(SeaBattle.id == sea_battle_id).first()
+    sea_battle_id: int, body: SeaBattleShipPayload, db: Session = Depends(get_db)
+) -> SeaBattleShipResponse:
+    sea_battle = db.query(SeaBattle).where(
+        SeaBattle.id == sea_battle_id).first()
     if sea_battle is None:
         raise HTTPException(status_code=404, detail="Sea Battle not found")
     sea_battle_ship = SeaBattleShip(
-        sea_battle_id = sea_battle_id,
-        Navy = NavyArray.index(body.Navy.name),
-        Type = ShipTypeArray.index(body.ShipType.name),
-        Size = body.Size,
-        Sunk = False
+        sea_battle_id=sea_battle_id,
+        Navy=NavyArray.index(body.Navy.name),
+        Type=ShipTypeArray.index(body.ShipType.name),
+        Size=body.Size,
+        Sunk=False
     )
     db.add(sea_battle_ship)
     db.commit()
@@ -93,45 +101,48 @@ async def sea_battle_create_ship(
     if body.Navy == Navy.Player:
         for point in body.Points:
             sbsgp = SeaBattleShipGridPoint(
-                sea_battle_ship_id = sea_battle_ship.id,
-                Horizontal = point.Horizontal,
-                Vertical = point.Vertical
+                sea_battle_ship_id=sea_battle_ship.id,
+                Horizontal=point.Horizontal,
+                Vertical=point.Vertical
             )
             db.add(sbsgp)
             db.commit()
     else:
-        points = OpponentShipPoints(db,sea_battle_id,sea_battle.Axis,body.Size)
+        points = OpponentShipPoints(
+            db, sea_battle_id, sea_battle.Axis, body.Size)
         for point in points:
             sbsgp = SeaBattleShipGridPoint(
-                sea_battle_ship_id = sea_battle_ship.id,
-                Horizontal = point["Horizontal"],
-                Vertical = point["Vertical"]
+                sea_battle_ship_id=sea_battle_ship.id,
+                Horizontal=point["Horizontal"],
+                Vertical=point["Vertical"]
             )
             db.add(sbsgp)
             db.commit()
     db.refresh(sea_battle_ship)
     return sea_battle_ship.as_dict()
 
+
 @router.post("/{sea_battle_id}/fire", status_code=201)
 async def sea_battle_fire(
-            sea_battle_id: int, body: SeaBattleFire, db: Session = Depends(get_db)
-        ) -> SeaBattleTurnResponse:
-    sea_battle = db.query(SeaBattle).where(SeaBattle.id == sea_battle_id).first()
+    sea_battle_id: int, body: SeaBattleFire, db: Session = Depends(get_db)
+) -> SeaBattleTurnResponse:
+    sea_battle = db.query(SeaBattle).where(
+        SeaBattle.id == sea_battle_id).first()
     if sea_battle is None:
         raise HTTPException(status_code=404, detail="Sea Battle not found")
     point: SeaBattlePoint | None = None
     if body.Navy == Navy.Player:
         point = SeaBattlePoint(
-            Horizontal = body.Horizontal,
-            Vertical = body.Vertical
+            Horizontal=body.Horizontal,
+            Vertical=body.Vertical
         )
     else:
-        point = OpponentFirePoint(db,sea_battle_id,sea_battle.Axis)
+        point = OpponentFirePoint(db, sea_battle_id, sea_battle.Axis)
     target: Target = Target.Miss
     shipType: int | None = None
     ships = db.query(SeaBattleShip).where(
-        SeaBattleShip.sea_battle_id == sea_battle_id and SeaBattleShip.Navy != NavyArray.index(body.Navy.name)
-    ).all()
+        SeaBattleShip.sea_battle_id == sea_battle_id
+    ).filter(SeaBattleShip.Navy != NavyArray.index(body.Navy.name)).all()
     for ship in ships:
         for p in ship.points:
             if point.Horizontal == p.Horizontal and point.Vertical == p.Vertical:
@@ -141,21 +152,29 @@ async def sea_battle_fire(
                     target = Target.Hit
                 shipType = ship.Type
                 hit = SeabattleShipHit(
-                    sea_battle_ship_id = ship.id,
-                    Horizontal = point.Horizontal,
-                    Vertical = point.Vertical
+                    sea_battle_ship_id=ship.id,
+                    Horizontal=point.Horizontal,
+                    Vertical=point.Vertical
                 )
                 db.add(hit)
                 db.commit()
+                if target == Target.Sunk:
+                    ship.Sunk = True
+                    db.add(ship)
+                    db.commit()
     turn = SeaBattleTurn(
-        sea_battle_id = sea_battle_id,
-        Navy = NavyArray.index(body.Navy.name),
-        Target = TargetArray.index(target.name),
-        ShipType = shipType,
-        Horizontal = point.Horizontal,
-        Vertical = point.Vertical
+        sea_battle_id=sea_battle_id,
+        Navy=NavyArray.index(body.Navy.name),
+        Target=TargetArray.index(target.name),
+        ShipType=shipType,
+        Horizontal=point.Horizontal,
+        Vertical=point.Vertical
     )
     db.add(turn)
     db.commit()
     db.refresh(turn)
+    status = SeaBattleStatus(db, sea_battle_id)
+    print("status", status.name)
+    if status.name != "Playing":
+        UpdateSeaBattle(db, sea_battle_id, status)
     return turn.as_dict()
--- utilities/sea_battle.py
+++ utilities/sea_battle.py
@@ -3,11 +3,16 @@ from sqlalchemy.orm import Session
 from payloads.sea_battle_payload import SeaBattlePoint
 from models.sea_battle_ship import SeaBattleShip
 from models.sea_battle_turn import SeaBattleTurn
+from models.sea_battle import SeaBattle
 import random
+from models.enums import GameStatus, GameStatusArray
 
-MaxAxisH = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-MaxAxisV = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
-Directions = ["N","S","E","W"]
+MaxAxisH = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+            "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+MaxAxisV = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+            14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
+Directions = ["N", "S", "E", "W"]
+
 
 def EmptyGrid(axis: int):
     grid = {}
@@ -17,36 +22,40 @@ def EmptyGrid(axis: int):
             grid[MaxAxisH[idxH]][MaxAxisV[idxV]] = False
     return grid
 
+
 def OpponentFirePoint(db: Session, id: int, axis: int):
     point: SeaBattlePoint | None = None
     grid = EmptyGrid(axis)
-    turns = db.query(SeaBattleTurn).where(SeaBattleTurn.sea_battle_id == id and SeaBattleTurn.Navy == 1).all()
+    turns = db.query(SeaBattleTurn).where(
+        SeaBattleTurn.sea_battle_id == id and SeaBattleTurn.Navy == 1).all()
     for turn in turns:
         grid[turn.Horizontal][turn.Vertical] = True
     found = False
     while not found:
-        idxH = random.randint(0,axis-1)
-        idxV = random.randint(0,axis-1)
+        idxH = random.randint(0, axis-1)
+        idxV = random.randint(0, axis-1)
         if not grid[MaxAxisH[idxH]][MaxAxisV[idxV]]:
             point = SeaBattlePoint(
-                Horizontal = MaxAxisH[idxH],
-                Vertical = MaxAxisV[idxV]
+                Horizontal=MaxAxisH[idxH],
+                Vertical=MaxAxisV[idxV]
             )
             found = True
     return point
 
+
 def OpponentShipPoints(db: Session, id: int, axis: int, size: int):
     points: List[SeaBattlePoint] = []
     grid = EmptyGrid(axis)
-    ships = db.query(SeaBattleShip).where(SeaBattleShip.sea_battle_id == id and SeaBattleShip.Navy == 1).all()
+    ships = db.query(SeaBattleShip).where(
+        SeaBattleShip.sea_battle_id == id and SeaBattleShip.Navy == 1).all()
     for ship in ships:
         for p in ship.points:
             grid[p.Horizontal][p.Vertical] = True
     while len(points) < size:
         points = []
         counter = 0
-        idxH = random.randint(0,axis-1)
-        idxV = random.randint(0,axis-1)
+        idxH = random.randint(0, axis-1)
+        idxV = random.randint(0, axis-1)
         direction = random.choice(Directions)
         while counter < size:
             if idxH < 0 or idxH >= axis or idxV < 0 or idxV >= axis:
@@ -55,7 +64,7 @@ def OpponentShipPoints(db: Session, id: int, axis: int, size: int):
             Vertical = MaxAxisV[idxV]
             if grid[MaxAxisH[idxH]][MaxAxisV[idxV]]:
                 break
-            points.append({ "Horizontal": Horizontal, "Vertical": Vertical})
+            points.append({"Horizontal": Horizontal, "Vertical": Vertical})
             counter = counter + 1
             if direction == "N":
                 idxV = idxV - 1
@@ -66,3 +75,54 @@ def OpponentShipPoints(db: Session, id: int, axis: int, size: int):
             else:
                 idxH = idxH - 1
     return points
+
+
+def SeaBattleStatus(db: Session, id: int):
+    status = GameStatus.Playing
+    sunk = {"Player": True, "Opponent": True}
+    ships = db.query(SeaBattleShip).where(
+        SeaBattleShip.sea_battle_id == id).all()
+    for ship in ships:
+        if ship.Navy == 0 and not ship.Sunk:
+            sunk["Player"] = False
+        elif ship.Navy == 1 and not ship.Sunk:
+            sunk["Opponent"] = False
+    if sunk["Opponent"]:
+        status = GameStatus.Won
+    elif sunk["Player"]:
+        status = GameStatus.Lost
+    return status
+
+
+def UpdateSeaBattle(db: Session, id: int, status: GameStatus):
+    score = 0
+    sea_battle = db.query(SeaBattle).where(SeaBattle.id == id).first()
+    if sea_battle is None:
+        return
+    maxTurns = sea_battle.Axis * sea_battle.Axis * 2
+    perMiss = 5
+    perHit = 10
+    if status == GameStatus.Won:
+        score = maxTurns * perMiss
+    turns = db.query(SeaBattleTurn).where(
+        SeaBattleTurn.sea_battle_id == id).all()
+    for turn in turns:
+        score = score - perMiss
+        if turn.Navy == 0:
+            if turn.Target == 0:
+                score = score - perMiss
+            elif turn.Target == 1:
+                score = score + perHit
+            elif turn.Target == 2:
+                score = score + (perHit * 2)
+        elif turn.Navy == 1:
+            if turn.Target == 0:
+                score = score + perMiss
+            elif turn.Target == 1:
+                score = score - perHit
+            elif turn.Target == 2:
+                score = score - (perHit * 2)
+    sea_battle.Score = score
+    sea_battle.Status = GameStatusArray.index(status.name)
+    db.add(sea_battle)
+    db.commit()
